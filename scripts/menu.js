d0ca0cb57fbf708e3922f70313495d441cfbc671
Jonatan <j.lorentzson@outlook.com>
2024-11-21 18:27:01 +0100

    add newsletter fix

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,22 @@
+//Newsletter window
+
+const closeNewsBtn = document.querySelector(".close-news-btn");
+const openNewsBtn = document.querySelector(".newsletterLink");
+openNewsBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    const menu = document.querySelector(".news-overlay");
+    menu.classList.toggle("menu-show");
+});
+
+closeNewsBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    const menu = document.querySelector(".news-overlay");
+    menu.classList.toggle("menu-show");
+});
+
+
+
+//Mobile menu
 const closeBtn = document.querySelector(".close-menu-btn");
 const openMenu = document.querySelector(".nav-button-menu");
 
